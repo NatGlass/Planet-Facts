a97035181a037c65daa5dbad987be0765ddf185d
Natr <nathanglass26@gmail.com>
2022-01-02 23:06:00 +0000

    started routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+import Header from './components/header'
+// pages //
+import Mercury from './pages/mercury'
+import Venus from './pages/venus'
+
 export default function App() {
   return (
-    <>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Mercury />}></Route>
+          <Route path="venus" element={<Venus />}></Route>
+          </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
