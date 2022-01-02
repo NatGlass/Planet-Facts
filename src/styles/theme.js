7ddecbb00c39875c6a550c279b422556e3dd5225
Natr <nathanglass26@gmail.com>
2022-01-02 18:53:27 +0000

    theme

---
+++ src/styles/theme.js
@@ -0,0 +1,44 @@
+export default {
+    colors: {
+        black: 'rgb(7,7,36)',
+        greyDark: 'rgb(56,56,79)',
+        grey: 'rgb(131,131,145)',
+        blueLight: 'rgb(65,158,187)',
+        yellow: 'rgb(237,162,73)',
+        purple: 'rgb(109,46,213)',
+        orangeLight: 'rgb(209,76,50)',
+        red: 'rgb(216,58,52)',
+        orange: 'rgb(205,81,32)',
+        green: 'rgb(30,193,162)',
+        blue: 'rgb(45,104,240)'
+    },
+    fonts: {
+        antonio: 'Antonio, sans-serif',
+        spartan: 'Spartan, sans-serif'
+    },
+    fontSettings: {
+            h1: {
+                size: '80px',
+                line: '103px',
+            },
+            h2: {
+                size: '40px',
+                line: '52px',
+                letter: '-1.5px',
+            },
+            h3: {
+                size: '12px', 
+                line: '25px',
+                letter: '2.6px',
+            },
+            h4: {
+                size: '11px', 
+                line: '25px',
+                letter: '1px',
+            },
+            p: {
+                size: '14px',
+                line: '25px',
+            },
+        }
+    }
