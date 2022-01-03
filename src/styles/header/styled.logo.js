feb38e5e10d7e61270d86687bf823dc0790a200f
Natr <nathanglass26@gmail.com>
2022-01-03 12:45:01 +0000

    reposition logo with margin

--- src/styles/header/styled.logo.js
+++ src/styles/header/styled.logo.js
@@ -4,6 +4,12 @@ export default styled.span`
     color: #fff;
     font-size: 28px;
     font-family: ${({ theme }) => theme.fonts.antonio};
+
+    // mobile //
+    @media screen and (max-width: 650px){
+        margin: 30px 0 0 16px;
+
+    }
     
     // tablet //
     @media screen and (min-width:651px) and (max-width:999px){
