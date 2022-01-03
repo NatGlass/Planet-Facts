ccce1a681ccf3f08731d90ea29f3e644149e951d
Natr <nathanglass26@gmail.com>
2022-01-03 14:20:05 +0000

    tabs link styles

---
+++ src/styles/shared/tab-links/styled.earth-tab-links.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+
+export default styled.span`
+    position: relative;
+    font-family: ${({ theme }) => theme.fonts.spartan};
+    color: #fff;
+    opacity: 0.5;
+    cursor: pointer;
+
+    // tablet //
+    @media screen and (min-width:651px) and (max-width:999px){
+        width: 100%;
+        height: 40px;
+        border: 1px solid rgba(255, 255, 255,.2);
+        padding: 14px 0 0 60px;
+        opacity: 1;
+        font-size: 10px;
+        letter-spacing: 1.93px;
+    }
+
+    // desktop //
+    @media screen and (min-width:1000px){
+        width: 100%;
+        height: 48px;
+        border: 1px solid rgba(255, 255, 255,.2);
+        padding: 19px 0 0 60px;
+        opacity: 1;
+        font-size: 12px;
+        letter-spacing: 2.57px;
+
+        &:hover{
+            background: rgba(216, 216, 216, 0.2);
+        }
+    }
+
+    // numbers //
+    @media screen and (min-width:651px){
+        &::after{
+            content: '';
+            position: absolute;
+            left: 25px;
+            opacity: 0.5;
+        }
+
+        &:nth-child(1)::after{
+            content: '01';
+        }
+
+        &:nth-child(2)::after{
+            content: '02';
+        }
+
+        &:nth-child(3)::after{
+            content: '03';
+        }
+    }
+
+    // active tab //
+    @media screen and (min-width:651px){
+        &.active-tab-link{
+            background: ${({ theme}) => theme.colors.purple}
+        }
+}
+
+
+`
