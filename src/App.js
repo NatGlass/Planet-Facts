0a1c2391bca7d7bf0b07884f8dd1e086a5d68e04
Natr <nathanglass26@gmail.com>
2022-01-04 14:02:08 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
@@ -17,19 +17,17 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Mercury />}></Route>
-          <Route path="venus" element={<Venus />}></Route>
-          <Route path="earth" element={<Earth />}></Route>
-          <Route path="mars" element={<Mars />}></Route>
-          <Route path="jupiter" element={<Jupiter />}></Route>
-          <Route path="saturn" element={<Saturn />}></Route>
-          <Route path="uranus" element={<Uranus />}></Route>
-          <Route path="neptune" element={<Neptune />}></Route>
+          <Route exact path="/" element={<Mercury />}></Route>
+          <Route path="/venus" element={<Venus />}></Route>
+          <Route path="/earth" element={<Earth />}></Route>
+          <Route path="/mars" element={<Mars />}></Route>
+          <Route path="/jupiter" element={<Jupiter />}></Route>
+          <Route path="/saturn" element={<Saturn />}></Route>
+          <Route path="/uranus" element={<Uranus />}></Route>
+          <Route path="/neptune" element={<Neptune />}></Route>
           </Routes>
-      </Router>
     </ThemeProvider>
   );
 }
